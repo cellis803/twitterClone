ea7dde8f9d84f2a65a91b474341c42adedb09dea
cellis803 <cellis803@gmail.com>
2016-09-08 09:46:48 -0400

    adding tests

---
+++ test/testApp.js
@@ -0,0 +1,12 @@
+var sum = require('../index');
+
+var chai = require('chai');
+var assert = chai.assert;
+
+
+describe('test nothing', function () {
+
+    it('should pass', function () {
+        assert.isTrue(true);
+    })
+})
