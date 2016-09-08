cad22d28b41c083d1d10b533ab98aa6599ee9721
zhan16 <hanzycy@yahoo.com>
2016-09-08 09:44:49 -0400

    test sync

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 function initDB(db) {
     db.serialize(function () {
-        db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='lorem'", function (error, row) {
+        db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='user'", function (error, row) {
             if (row !== undefined) {
                 console.log("table exists. cleaning existing records");
                 // db.run("DELETE FROM lorem", function (error) {
@@ -9,27 +9,30 @@ function initDB(db) {
                 // });
             }
             else {
-                console.log("creating table")
-                db.run("CREATE TABLE lorem (info TEXT)", function (error) {
-                    if (error.message.indexOf("already exists") != -1) {
-                        console.log(error);
-                    }
-                });
+                console.log("creating tables");
+
+                db.run("CREATE TABLE user (name TEXT)");
+                db.run("CREATE TABLE tweet (userId INTEGER, tweetText TEXT, time DATETIME, parentTweetId INTEGER)");
+                db.run("CREATE TABLE userFollows (followsUserId INTEGER)");
+                db.run("CREATE TABLE tweetReplies (tweetId INTEGER, replyText TEXT, replyUserId INTEGER)");
+                db.run("CREATE TABLE tweetLikes (likeUserId INTEGER)");
+
+                console.log("tables have been created");
             }
         });
 
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-        for (var i = 0; i < 10; i++) {
-            stmt.run("Ipsum " + i, function (error) {
-                if (error)
-                    console.log(error);
-            });
-        }
-        stmt.finalize();
+        // var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        // for (var i = 0; i < 10; i++) {
+        //     stmt.run("Ipsum " + i, function (error) {
+        //         if (error)
+        //             console.log(error);
+        //     });
+        // }
+        // stmt.finalize();
 
-        db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-            console.log(row.id + ": " + row.info);
-        });
+        // db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+        //     console.log(row.id + ": " + row.info);
+        // });
 
         //db.close();
     });
